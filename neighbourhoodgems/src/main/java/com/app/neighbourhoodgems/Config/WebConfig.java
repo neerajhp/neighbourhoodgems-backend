be0343d82fc4a26689cabed89779e5beceef4486
Neeraj Patel <neerajhpatel@gmail.com>
2022-02-10 20:11:16 +1100

    Added Spring WebMVC Config to enable CORS

---
+++ neighbourhoodgems/src/main/java/com/app/neighbourhoodgems/Config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.app.neighbourhoodgems.Config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/** Spring MVC Configuration @Author Neeraj Patel */
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+  @Value("${app.cors.allowedOrigins}")
+  private String[] allowedOrigins;
+
+  /** Enable CORS for */
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry
+        .addMapping("/**")
+        .allowedOrigins(allowedOrigins)
+        .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE");
+  }
+}
